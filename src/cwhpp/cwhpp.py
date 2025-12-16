482d9ae67577299e6b2063debabb30f4f3f43427
k2kfed <olivier.meslin@insee.fr>
2025-12-16 09:25:48 +0000

    Clean

--- src/cwhpp/cwhpp.py
+++ src/cwhpp/cwhpp.py
@@ -834,20 +834,6 @@ but the name of the floor area variable is missing")
             assert isinstance(obj, np.ndarray), "Object is not a numpy array"
             assert obj.ndim == 1, "Array is not 1-dimensional"
 
-    def complete_df(
-        self,
-        df: pl.DataFrame,
-        columns: list[str]
-    ):
-        uniq = [df.select(pl.col(c).unique()) for c in columns]
-
-        # start grid
-        grid = uniq[0]
-        for u in uniq[1:]:
-            grid = grid.join(u, how="cross")
-
-        return grid.join(df, on=columns, how="left")
-
     def calibrate_model(
         self,
         X: pl.DataFrame = None,
